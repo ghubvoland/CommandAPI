e030f39368ca998882f6dd555f985360ebd67c5e
Lenny M Miranda <mmmlmmm@yahoo.com>
2021-10-28 18:27:20 -0400

    Con datos Postgre

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -8,19 +8,29 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace CommandAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration {get;}
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CommandContext>(opt => opt.UseNpgsql
+                (Configuration.GetConnectionString("PostgreSqlConnection")));
             //SECTION 1. Add code below
             services.AddControllers();
             //Add the code below
-            services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+            //services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+            services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
             // Add framework services SUGERENCIA QUE SIRVIÖ !!!!! >>>
             services.AddMvc()
                     .AddControllersAsServices();      // <---- Super important
